f62a35fd08e873c083cfbd521df63527415c8471
buterbrot359 <batowka359@gmail.com>
2026-02-17 21:29:11 +0500

    feat: test locale

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
-import Icon from '@/shared/assets/react.svg'
 import { GreenAccentButton } from '@/shared/ui/buttons/green-accent-button'
+import { useTranslation } from 'react-i18next'
 
 function App() {
+  const { t, i18n } = useTranslation()
+
   return (
     <div className="bg-ui-06 w-full h-screen">
-      <Icon />
-      <GreenAccentButton>1231</GreenAccentButton>
+      <div className="bg-ui-01">{t('actions.back')}</div>
+      <GreenAccentButton
+        onClick={() =>
+          i18n.changeLanguage(i18n.language === 'ru' ? 'kz' : 'ru')
+        }
+      >
+        1231
+      </GreenAccentButton>
     </div>
   )
 }

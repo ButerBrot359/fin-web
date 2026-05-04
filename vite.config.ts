e6ad6b452e0c24b0a91ab70bd8b13787ca6651fc
Nurlan Muldashev <nurlan@muldashev.kz>
2026-05-04 17:58:48 +0500

    feat: новый деплой

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: ['dev.qazyna.ai', 'demo.qazyna.ai', 'qazyna.ai'],
+  },
 })
